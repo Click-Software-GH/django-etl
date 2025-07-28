6bf9150225017e3b85fc67e4f09316fee212ecae
teflon40 <frimpong.bernard3220@gmail.com>
2025-07-28 06:40:33 +0000

    Fix import path for DataCleaner and HashGenerator in ETL utility script

--- django_etl/utils.py
+++ django_etl/utils.py
@@ -16,7 +16,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.local")
 django.setup()
 
 from django.db import connections
-from migration_core.etl_helpers import DataCleaner, HashGenerator
+from .helpers import DataCleaner, HashGenerator
 import argparse
 import json
 
